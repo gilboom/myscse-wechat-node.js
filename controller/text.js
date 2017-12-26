1bfa2847a1c053b3f2474579e20a1bd5c893c961
gil <q86002618@163.com>
2017-12-26 22:17:00 +0800

    增加提取绑定信息功能

---
+++ controller/text.js
@@ -0,0 +1,34 @@
+const textTemplate = require('../template/textTemplate')
+module.exports = function (msg,res) {
+
+        const _studentInfo = msg.Content.trim()
+        let response,content
+        if(!_studentInfo.test('帐号') || !_studentInfo.test('密码') ) {
+                content = '你输入的绑定信息不规范，请重试！'
+                response = textTemplate(msg,content)
+        }else {
+                const username = getUsername(_studentInfo)
+                const password = getPassword(_studentInfo)
+                const openId = msg.FromUserName
+                content = 
+                `你绑定的信息是：
+                用户名：${username}
+                密码：${password}
+                openID：${openId}
+                `
+                response = textTemplate(msg,content)
+        } 
+        res.send(response)
+
+}
+
+function getUsername(str) {
+        const start  = str.search(/帐号/)+2
+        const end = str.search(/密码/)-1
+        return str.substring(start,end).trim()
+}
+
+function getPassword(str) {
+        const start = str.search(/密码/)+2
+        return str.substring(start).trim()
+}
